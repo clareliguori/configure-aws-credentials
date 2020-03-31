c1ffbc47e6bf5dc25ae4bd542d28b16fb5bec793
Clare Liguori <liguori@amazon.com>
2020-03-31 14:06:17 -0700

    For debugging, don't mask access key (it's set as fake strings anyway)

--- index.js
+++ index.js
@@ -95,12 +95,12 @@ function exportCredentials(params){
   // AWS_ACCESS_KEY_ID:
   // Specifies an AWS access key associated with an IAM user or role
   core.exportVariable('AWS_ACCESS_KEY_ID', accessKeyId);
-  core.setSecret(accessKeyId);
+  //core.setSecret(accessKeyId);
 
   // AWS_SECRET_ACCESS_KEY:
   // Specifies the secret key associated with the access key. This is essentially the "password" for the access key.
   core.exportVariable('AWS_SECRET_ACCESS_KEY', secretAccessKey);
-  core.setSecret(secretAccessKey);
+  //core.setSecret(secretAccessKey);
 
   // AWS_SESSION_TOKEN:
   // Specifies the session token value that is required if you are using temporary security credentials.
